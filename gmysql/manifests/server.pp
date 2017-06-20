6431631dd573b595cd095298f6d23646c52c5ace
root <root@dhcp-flab2-54.cesnet.cz>
2017-06-20 17:56:56 +0200

    Task #3533 HaaS: module gmysql remove gc legacy backup code

--- gmysql/manifests/server.pp
+++ gmysql/manifests/server.pp
@@ -2,14 +2,11 @@
 #
 # TODO documentation
 #
-class gmysql::server(
-	$mysql_buser = "bckup",
-	$mysql_buser_wpass = undef,
- 	$mysql_nuser = "nagios",
-	$mysql_nuser_wpass = undef,
-) {
+class gmysql::server() {
         notice("INFO: pa.sh -v --noop --show_diff -e \"include ${name}\"")
 
+
+	# install
 	class { '::mysql::server':
 		package_name => "mariadb-server",
 		service_provider => "debian",
@@ -18,6 +15,8 @@ class gmysql::server(
 	}
 	package { "mysqltuner": ensure => installed, }
 
+
+	# hardening
 #maria db authenticated by socket
 #	mysql_user { [ "root@$hostname", "root@127.0.0.1", "root@::1", "root@localhost", "root@localhost.localdomain"]:
 #	        ensure => 'absent',
@@ -28,8 +27,17 @@ class gmysql::server(
 		onlyif => "/usr/bin/mysql -NBe 'SHOW GRANTS FOR \"root\"@\"localhost\"' | /bin/grep PROXY",
 		require => Package["mysql-server"],
 	}
+	exec { "mysql passwordless accounts":
+                command => "/bin/sh /puppet/gmysql/bin/nopass.sh || true",
+		require => Package["mysql-server"],
+        }
+	cron { "mysql passwordless accounts":
+                command => "/bin/sh /puppet/gmysql/bin/nopass.sh",
+                user => root, hour => 0, minute => 1,
+        }
 
 
+	# config
 #	file { "/etc/mysql/mariadb.conf.d/90-miscgcm.cnf":
 #	        source => "puppet:///modules/${module_name}/etc/mysql/mariadb.conf.d/90-miscgcm.cnf",
 #	        owner => "root", group => "root", mode => "0644",
@@ -37,105 +45,25 @@ class gmysql::server(
 #		notify => Service["mysqld"],
 #	}
 
-	exec { "mysql passwordless accounts":
-                command => "/bin/sh /puppet/gmysql/bin/nopass.sh || true",
-		require => Package["mysql-server"],
-        }
 
-
-
-	if ($mysql_buser_wpass) {
-	        $mysql_buser_wpass_real = $mysql_buser_wpass
-	} else {
-	        if ( file_exists("/etc/bacula/scripts/mysql_buser.wpass") == 1 ) {
-	                $mysql_buser_wpass_real = myexec("/bin/cat /etc/bacula/scripts/mysql_buser.wpass")
-	        } else {
-	                $mysql_buser_wpass_real = myexec("/bin/dd if=/dev/urandom bs=100 count=1 2>/dev/null | /usr/bin/sha256sum | /usr/bin/awk '{print \$1}'")
-	                notice("INFO: mysql_buser.wpass secret generated")
-	        }
+	# backup
+	file { "/etc/bacula":
+		ensure => "directory",
+		owner => "root", group => "root", mode => "0644",
 	}
-	if ($mysql_buser_wpass_real) {
-	        file { "/etc/bacula":
-	                ensure => "directory",
-			owner => "root", group => "root", mode => "0644",
-	        }
-	        file { "/etc/bacula/scripts":
-	                ensure => "directory",
-	                require => File["/etc/bacula"],
-			owner => "root", group => "root", mode => "0640",
-	        }
-	        file { "/etc/bacula/scripts/mysql_buser.wpass":
-	                content => "$mysql_buser_wpass_real\n",
-	                owner => "root", group => "root", mode => "0600",
-	                require => File["/etc/bacula/scripts"],
-	        }
-	        file { "/etc/bacula/scripts/mysql_backup2.sh":
-	                source => "puppet:///modules/${module_name}/etc/bacula/scripts/mysql_backup2.sh",
-	                owner => "root", group => "root", mode => "0700",
-	                require => File["/etc/bacula/scripts"],
-	        }
-	        file { "/etc/bacula/scripts/mysql_backup2_del.sh":
-	                source => "puppet:///modules/${module_name}/etc/bacula/scripts/mysql_backup2_del.sh",
-	                owner => "root", group => "root", mode => "0700",
-	                require => File["/etc/bacula/scripts"],
-	        }
-	
-	        mysql_user { "${mysql_buser}@localhost":
-	                ensure => present,
-	                password_hash => mysql_password($mysql_buser_wpass_real),
-	        }
-	        mysql_grant { "${mysql_buser}@localhost/*.*":
-	                ensure     => present,
-	                privileges => ["SELECT", "LOCK TABLES", "SHOW VIEW", "TRIGGER", "PROCESS", "EVENT", "SUPER"],
-	                table      => '*.*',
-	                user       => "${mysql_buser}@localhost",
-			require => Mysql_user["${mysql_buser}@localhost"],
-
- 	        }
-
-	} else {
-	        warning("SKIPPED mysql bacula backup scripts facts missing")
+	file { "/etc/bacula/scripts":
+		ensure => "directory",
+	        require => File["/etc/bacula"],
+		owner => "root", group => "root", mode => "0640",
 	}
-
-
-
-	if ($mysql_nuser_wpass) {
-	        $mysql_nuser_wpass_real = $mysql_nuser_wpass
-	} else {
-	        if ( file_exists("/etc/mysql/mysql_nuser.wpass") == 1 ) {
-	                $mysql_nuser_wpass_real = myexec("/bin/cat /etc/mysql/mysql_nuser.wpass")
-	        } else {
-	                $mysql_nuser_wpass_real = myexec("/bin/dd if=/dev/urandom bs=100 count=1 2>/dev/null | /usr/bin/sha256sum | /usr/bin/awk '{print \$1}'")
-	                notice("INFO: mysql_nuser.wpass secret generated")
-	        }
+	file { "/etc/bacula/scripts/mysql_backup2.sh":
+		source => "puppet:///modules/${module_name}/etc/bacula/scripts/mysql_backup2.sh",
+		owner => "root", group => "root", mode => "0700",
+	        require => File["/etc/bacula/scripts"],
 	}
-	if ($mysql_nuser_wpass_real) {
-	        file { "/etc/mysql/mysql_nuser.wpass":
-	                content => "$mysql_nuser_wpass_real\n",
-	                owner => "root", group => "root", mode => "0600",
-	        }
-	        mysql_user { "${mysql_nuser}@localhost":
-	                ensure => present,
-	                password_hash => mysql_password($mysql_nuser_wpass_real),
-	        }
-        	mysql_grant { "${mysql_nuser}@localhost/*.*":
-	                ensure     => present,
-	                privileges => ["USAGE"],
-	                table      => '*.*',
-	                user       => "${mysql_nuser}@localhost",
-			require => Mysql_user["${mysql_nuser}@localhost"],
-	        }
-	} else {
-	        warning("SKIPPED mysql nagios facts missing")
+	file { "/etc/bacula/scripts/mysql_backup2_del.sh":
+		source => "puppet:///modules/${module_name}/etc/bacula/scripts/mysql_backup2_del.sh",
+		owner => "root", group => "root", mode => "0700",
+	        require => File["/etc/bacula/scripts"],
 	}
-
-
-	cron { "mysql passwordless accounts":
-                command => "/bin/sh /puppet/gmysql/bin/nopass.sh",
-                user => root,
-                hour => 0,
-                minute => 1,
-        }
-
 }
-
