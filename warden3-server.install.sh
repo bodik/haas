a1fc8468e6124a7c1735061eabe62e1d55475e66
root <root@dhcp-flab2-52.cesnet.cz>
2017-06-29 19:10:50 +0200

    bootstrap server with at least one message

--- warden3-server.install.sh
+++ warden3-server.install.sh
@@ -1,5 +1,18 @@
 
 pa.sh -e 'include warden3::ca'
 pa.sh -e 'include warden3::server'
+pa.sh -e 'include warden3::tester'
 
+# exception to register sensor here, it's much easier to make it after wserver running, than requiring all components in manifest
 /bin/sh /puppet/warden3/bin/register_sensor.sh -s $(facter fqdn) -n "cz.cesnet.flab.$(facter hostname).puppet_test_client" -d /opt/warden_server
+
+# I bet this solves caveat chicked-egg
+# have a clean server >> zero events in db, have a receiving client (tologstash) with not existing warden_client last_event_id file
+# so until at least one event comes in, last_events holds 0 for the client and wserver thinks client wants to reset lastReceivedId (getEvents id<=0 block)
+# some event comes in from sender, highest event.id becomes > 0
+# receiver comes again, with id 0 because of previous 0 items in queue, not that he wants to reset last_received_id, but server resets and receiver misses some of the first messages
+EVENTS=$(mysql -NBe 'select count(*) from events;' warden3)
+if [ $EVENTS -eq 0 ]; 
+	python /opt/warden_tester/bootstrap_server.py
+fi
+
