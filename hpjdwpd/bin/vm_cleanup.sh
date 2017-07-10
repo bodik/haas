c91e0115b3747ffc61381695f4214845de2d5ca6
root <root@dhcp-flab2-50.cesnet.cz>
2017-07-10 21:28:20 +0200

    hpjdpwd cleanup init

---
+++ hpjdwpd/bin/vm_cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+systemctl stop jdwpd
+find /opt/hostcert -type f -exec shred --force --remove {} \;
+find /opt/jdwpd/log -type f -exec shred --force --remove {} \;
+find /opt/jdwpd/bin -type f -name '*cfg' -exec shred --force --remove {} \;
+truncate --size 0 /opt/jdwpd/bin/registered-at-warden-server
+
