c8759b7f6d8964f8401b032b1df4b33a25feec51
root <root@dhcp-flab2-49.cesnet.cz>
2017-07-12 00:36:00 +0200

    fixed errors made by refactoring

--- warden3/bin/register_sensor.sh
+++ warden3/bin/register_sensor.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 # will register sensor at warden server
 
-CA_SERVICE="http://${WARDEN_SERVER}:45444"
-
-
 rreturn() { echo "$2"; exit $1; }
 usage() { echo "Usage: $0 -s <WARDEN_SERVER> -n <SENSOR_NAME> -d <DESTDIR>" 1>&2; exit 1; }
 while getopts "s:n:d:" o; do
@@ -18,12 +15,12 @@ shift "$(($OPTIND-1))"
 test -n "$WARDEN_SERVER" || rreturn 1 "ERROR: missing WARDEN_SERVER"
 test -n "$SENSOR_NAME" || rreturn 1 "ERROR: missing SENSOR_NAME"
 test -n "$DESTDIR" || rreturn 1 "ERROR: missing DESTDIR"
+CA_SERVICE="http://${WARDEN_SERVER}:45444"
 
 
 
 #if tagfile exist, a sensor is probably already registred, this is just puppet helper conditional
-test -f "${DESTDIR}/registered-at-warden-server" || exit 0
-
+if [ -f "${DESTDIR}/registered-at-warden-server" ]; then exit 0; fi
 
 URL="${CA_SERVICE}/register_sensor?sensor_name=${SENSOR_NAME}"
 curl --silent --write-out '%{http_code}' "${URL}" | grep 200 1>/dev/null 2>/dev/null
@@ -34,4 +31,3 @@ else
 	echo "ERROR: cannt register at warden server"
 	exit 1
 fi
-
