4d65abd4be9a402ee65a2ac3a7af8de5983fcd63
root <root@dhcp-flab2-50.cesnet.cz>
2017-07-11 16:58:51 +0200

    removing not used hostspecs feature

--- check_stddev.sh
+++ check_stddev.sh
@@ -1,13 +1,8 @@
 #!/bin/sh 
 
-if [ -f /puppet/hostspecs/PRIVATEFILE_host_$(facter fqdn).pp ]; then
-	echo "INFO: /puppet/metalib/bin/pa.sh -v --noop --show_diff /puppet/hostspecs/PRIVATEFILE_host_$(facter fqdn).pp"
-	/puppet/metalib/bin/pa.sh -v --noop --show_diff /puppet/hostspecs/PRIVATEFILE_host_$(facter fqdn).pp
-else
-	for all in $(find . -maxdepth 2 -type f -name "*.check.sh"); do
-		sh $all
-	done
-fi
+for all in $(find . -maxdepth 2 -type f -name "*.check.sh"); do
+	sh $all
+done
 
 pa.sh -v --noop --show_diff -e "include metalib::puppet_cleanup"
 
--- hostspecs.check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh 
-
-if [ -e /puppet/hostspecs/host_$(facter fqdn).pp ]; then
-        echo "INFO: HOSTSPECIFICCHECK ======================="
-        echo "INFO: pa.sh -v --noop --show_diff /puppet/hostspecs/host_$(facter fqdn).pp"
-        pa.sh -v --noop --show_diff /puppet/hostspecs/host_$(facter fqdn).pp
-fi
-
