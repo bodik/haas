2e9a99a7575906c386117f8a505f1b41b5328e5b
root <root@dhcp-flab2-52.cesnet.cz>
2017-07-13 12:09:52 +0200

    preparation for x509nameauthenticator, eg. would install certificate for any requested name, hostname by default

--- warden3/bin/install_ssl_warden_ca.sh
+++ warden3/bin/install_ssl_warden_ca.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 DESTDIR="/opt/hostcert"
-FQDN=$(facter fqdn)
+NAME=$(facter fqdn)
 
 rreturn() { echo "$2"; exit $1; }
-usage() { echo "Usage: $0 -w <WARDEN_SERVER> -d <DESTDIR>" 1>&2; exit 1; }
-while getopts "w:d:" o; do
+usage() { echo "Usage: $0 -w WARDEN_SERVER -d DESTDIR [-n CLIENT_NAME]" 1>&2; exit 1; }
+while getopts "w:d:n:" o; do
 	case "${o}" in
         	w) WARDEN_SERVER=${OPTARG} ;;
 		d) DESTDIR=${OPTARG} ;;
+		n) NAME=${OPTARG} ;;
 		*) usage ;;
 	esac
 done
@@ -21,20 +22,20 @@ CA_SERVICE="http://${WARDEN_SERVER}:45444"
 
 mkdir -p ${DESTDIR}
 cd ${DESTDIR} || exit 1
-if [ -f ${FQDN}.crt ]; then
-        echo "WARN: certificate ${FQDN}.crt already present in ${DESTDIR}"
+if [ -f ${NAME}.crt ]; then
+        echo "WARN: certificate ${NAME}.crt already present in ${DESTDIR}"
 	find ${DESTDIR} -ls
 	exit 0
 fi
 
 
 
-echo "INFO: generating ${DESTDIR}/${FQDN}.key"
-openssl req -newkey rsa:4096 -nodes -keyout "${FQDN}.key" -out "${FQDN}.csr" -subj "/CN=${FQDN}/"
+echo "INFO: generating ${DESTDIR}/${NAME}.key"
+openssl req -newkey rsa:4096 -nodes -keyout "${FQDN}.key" -out "${NAME}.csr" -subj "/CN=${NAME}/"
 
-echo "INFO: signing ${FQDN}.csr"
+echo "INFO: signing ${NAME}.csr"
 #TODO: (in)secure
-curl --insecure --fail --data-urlencode @"${FQDN}.csr" "${CA_SERVICE}/put_csr"
+curl --insecure --fail --data-urlencode @"${NAME}.csr" "${CA_SERVICE}/put_csr"
 if [ $? -ne 0 ]; then
 	echo "ERROR: cannot contact warden_ca"
 	exit 1
@@ -42,13 +43,13 @@ fi
 
 SIGNED=0
 while [ ${SIGNED} -eq 0 ]; do
-	curl --insecure --output "${FQDN}.crt" "${CA_SERVICE}/get_crt" 2>/dev/null
-	openssl x509 -in "${FQDN}.crt" 1>/dev/null 2>/dev/null
+	curl --insecure --output "${NAME}.crt" "${CA_SERVICE}/get_crt" 2>/dev/null
+	openssl x509 -in "${NAME}.crt" 1>/dev/null 2>/dev/null
 	if [ $? -eq 0 ]; then
 		SIGNED=1
 	else
 		echo "INFO: waiting for warden_ca to sign the csr"
-		rm "${FQDN}.crt"
+		rm "${NAME}.crt"
 		sleep 1
 	fi
 done
@@ -58,4 +59,4 @@ curl --insecure --output "ca.crl" "${CA_SERVICE}/get_crl" 2>/dev/null
 
 find . -type f -exec chmod 644 {} \;
 
-echo "INFO: done generating certificate from warden_ca"
+echo "INFO: done generating ${NAME} certificate from warden_ca"
