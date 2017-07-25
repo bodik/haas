d2561cd10ad2a070b6f3adb2bbfffe491334d60c
root <root@dhcp-flab2-52.cesnet.cz>
2017-07-25 12:40:02 +0200

    Warden RA prepare script

---
+++ warden3/files/opt/warden_ra/prepare-ca.sh
@@ -0,0 +1,19 @@
+mkdir ca
+
+mkdir certs clients crl csr newcerts private
+chmod 700 private
+touch index.txt
+echo 1000 > serial
+
+#TODO copy openssl.cnf
+
+openssl genrsa -aes256 -passout pass:xxxxxx -out private/ca.key.pem 4096
+chmod 400 private/ca.key.pem
+
+openssl req -config openssl.cnf -passin pass:xxxxxx -key private/ca.key.pem -new -x509 -days 7300 -sha256 -extensions v3_ca -out certs/ca.cert.pem -subj "/C=CZ/ST=Czech Republic/L=Pilsen/O=FLAB/CN=warden-ra.flab.cesnet.cz"
+
+openssl rsa -in private/ca.key.pem -out private/ca.key2.pem -passin pass:xxxxxx
+mv private/ca.key2.pem private/ca.key.pem
+
+chmod 444 certs/ca.cert.pem
+
