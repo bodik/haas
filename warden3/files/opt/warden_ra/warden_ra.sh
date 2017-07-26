434b6662aed64d9073f6e0c6e224ed3a3d0248e7
root <root@dhcp-flab2-52.cesnet.cz>
2017-07-26 11:20:52 +0200

    ra tuning

--- warden3/files/opt/warden_ra/warden_ra.sh
+++ warden3/files/opt/warden_ra/warden_ra.sh
@@ -7,13 +7,11 @@ init() {
 	echo 1000 > serial
 	
 	openssl genrsa -aes256 -passout pass:xxxxxx -out private/ca.key.pem 4096
-	chmod 400 private/ca.key.pem
-	
-	openssl req -config openssl.cnf -passin pass:xxxxxx -key private/ca.key.pem -new -x509 -days 7300 -sha256 -extensions v3_ca -out certs/ca.cert.pem -subj "/C=CZ/ST=Czech Republic/L=Pilsen/O=FLAB/CN=warden-ra.flab.cesnet.cz"
-	
 	openssl rsa -in private/ca.key.pem -out private/ca.key2.pem -passin pass:xxxxxx
 	mv private/ca.key2.pem private/ca.key.pem
+	chmod 400 private/ca.key.pem
 	
+	openssl req -config openssl.cnf -passin pass:xxxxxx -key private/ca.key.pem -new -x509 -days 7300 -sha256 -extensions v3_ca -out certs/ca.cert.pem -subj "/CN=warden3ra"
 	chmod 444 certs/ca.cert.pem
 }
 
