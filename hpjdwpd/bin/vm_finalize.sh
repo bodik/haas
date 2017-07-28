5fce0a20587223e8cc98f06d5636e8fcf9c81f45
root <root@dhcp-flab2-40.cesnet.cz>
2017-07-28 13:46:09 +0200

    jdwpd finalize typo

--- hpjdwpd/bin/vm_finalize.sh
+++ hpjdwpd/bin/vm_finalize.sh
@@ -10,6 +10,6 @@ if [ -f ${INSTALL_DIR}/bin/jdwpd.py ]; then
 	if [ -z "${AUTOTEST}" ]; then
 		/puppet/jenkins/bin/haas_vm_finalize_racert.sh -i $INSTALL_DIR $@
 	fi
-	pa.sh -e "class { 'hpjdpwd': warden_server_url => '${WARDEN_SERVER_URL}', warden_client_name => '${CLIENT_NAME}' }"
+	pa.sh -e "class { 'hpjdwpd': warden_server_url => '${WARDEN_SERVER_URL}', warden_client_name => '${CLIENT_NAME}' }"
 fi
 
