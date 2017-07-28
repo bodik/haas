7fbe14f610852f766d22151cae39fd4faef445ae
root <root@dhcp-flab2-53.cesnet.cz>
2017-07-28 15:27:44 +0200

    generate_password optional param fix

--- metalib/lib/puppet/functions/generate_password.rb
+++ metalib/lib/puppet/functions/generate_password.rb
@@ -3,16 +3,16 @@ Puppet::Functions.create_function(:generate_password) do
 	#
 	# @return generated password
 	# @param outlen optional, length of the password to generate
-        def generate_password(outlen)
+        def generate_password(*outlen)
 
                 out = Facter::Util::Resolution.exec("/bin/dd if=/dev/urandom bs=100 count=1 2>/dev/null | /usr/bin/sha256sum | /usr/bin/awk '{print $1}'")
                 if out.nil?
                         return :undef
                 else
-			if outlen.nil?
+			if outlen.empty?
 	                        return out
 			else
-				return out[0, outlen]
+				return out[0, outlen[0]]
 			end
                 end
         end
