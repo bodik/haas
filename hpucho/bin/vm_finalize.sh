8a9e01df68d97f61a4d63b7bb4a85d47e519acc6
root <root@dhcp-flab2-37.cesnet.cz>
2017-07-31 15:32:15 +0200

    refactoring error fix

--- hpucho/bin/vm_finalize.sh
+++ hpucho/bin/vm_finalize.sh
@@ -17,6 +17,6 @@ if [ -f ${INSTALL_DIR}/bin/uchoudp.py ]; then
 	if [ -z "${AUTOTEST}" ]; then
 		/puppet/jenkins/bin/haas_vm_finalize_racert.sh -i $INSTALL_DIR -w $WARDEN_SERVER_URL -n "${CLIENT_NAME}.uchoudp"
 	fi
-	pa.sh -e "class { 'hpucho::tcp': warden_server_url => '${WARDEN_SERVER_URL}', warden_client_name => '${CLIENT_NAME}.uchoudp' }"
+	pa.sh -e "class { 'hpucho::udp': warden_server_url => '${WARDEN_SERVER_URL}', warden_client_name => '${CLIENT_NAME}.uchoudp' }"
 fi
 
