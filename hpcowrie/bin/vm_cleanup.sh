06dba44c7ddc0c7f0c9fe26c8bfd2c45303c8ed8
root <root@dhcp-flab2-38.cesnet.cz>
2017-08-02 10:08:39 +0200

    Cleanup cosmetics

--- hpcowrie/bin/vm_cleanup.sh
+++ hpcowrie/bin/vm_cleanup.sh
@@ -7,8 +7,6 @@ if [ -f ${INSTALL_DIR}/bin/cowrie ]; then
 	systemctl stop cowrie
 
 	find ${INSTALL_DIR}/racert -type f -exec shred --force --remove {} \;
-	truncate --size 0 ${INSTALL_DIR}/racert/registered-at-warden-server
-
 	find ${INSTALL_DIR} -maxdepth 1 -type f -name '*cfg' -exec shred --force --remove {} \;
 	find ${INSTALL_DIR}/warden -type f -name '*cfg' -exec shred --force --remove {} \;
 	find ${INSTALL_DIR}/log -type f -exec shred --force --remove {} \;
--- hpucho/bin/vm_cleanup.sh
+++ hpucho/bin/vm_cleanup.sh
@@ -5,7 +5,6 @@ if [ -f ${INSTALL_DIR}/bin/uchotcp.py ]; then
 
 	systemctl stop uchotcp
 	find ${INSTALL_DIR}/racert -type f -exec shred --force --remove {} \;
-	truncate --size 0 ${INSTALL_DIR}/racert/registered-at-warden-server
 	find ${INSTALL_DIR}/log -type f -exec shred --force --remove {} \;
 	find ${INSTALL_DIR}/bin -type f -name '*cfg' -exec shred --force --remove {} \;
 fi
@@ -15,7 +14,6 @@ if [ -f ${INSTALL_DIR}/bin/uchoudp.py ]; then
 
 	systemctl stop uchoudp
 	find ${INSTALL_DIR}/racert -type f -exec shred --force --remove {} \;
-	truncate --size 0 ${INSTALL_DIR}/racert/registered-at-warden-server
 	find ${INSTALL_DIR}/log -type f -exec shred --force --remove {} \;
 	find ${INSTALL_DIR}/bin -type f -name '*cfg' -exec shred --force --remove {} \;
 fi
