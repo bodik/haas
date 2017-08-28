dfadb2d18a25f225aae990441c02b29a8dbf941f
root <root@dhcp-flab3-254.cesnet.cz>
2017-08-28 15:17:35 +0200

    warden_server_allow_dash_client_name.patch application

--- warden3/files/opt/warden_server/warden_server.py
+++ warden3/files/opt/warden_server/warden_server.py
@@ -1447,7 +1447,7 @@ def modify_client(**kwargs):
             for label in hostname.split("."))
 
     def isValidNSID(nsid):
-        allowed = re.compile(r"^(?:[a-zA-Z_][a-zA-Z0-9_]*\.)*[a-zA-Z_][a-zA-Z0-9_]*$")
+        allowed = re.compile(r"^(?:[a-zA-Z_][a-zA-Z0-9_\-]*\.)*[a-zA-Z_][a-zA-Z0-9_\-]*$")
         return allowed.match(nsid)
 
     def isValidEmail(mail):
